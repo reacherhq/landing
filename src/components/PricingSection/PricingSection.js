dfbce0d9d7213cf5ef0633a3ef88470756eb281b
Amaury Martiny <amaury.martiny@protonmail.com>
2020-06-30 19:43:38 +0200

    More tweaks in Pricing

--- src/components/PricingSection/PricingSection.js
+++ src/components/PricingSection/PricingSection.js
@@ -12,6 +12,13 @@ import styles from './styles.module.css'
 const payg = plans[1].original.requests
 
 /**
+ * @see https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+ */
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+/**
  * Given the number of emails, calculate the price.
  */
 function calculatePrice(emails = 0) {
@@ -55,58 +62,70 @@ export class PricingSection extends Component {
               ...plans[1],
               context: null, // Remove top section hack.
               desc: (
-                <span>
-                  <label htmlFor='emails'>ESTIMATE LOOKUPS/MONTH:</label>
-                  <input
-                    className={styles.emailsInput}
-                    min={0}
-                    name='emails'
-                    onChange={({ target: { value } }) =>
-                      this.setState({ emails: value })
-                    }
-                    step={1}
-                    type='number'
-                    value={this.state.emails}
-                  />
-                </span>
+                <div>
+                  <span>
+                    <label htmlFor='emails'>ESTIMATE LOOKUPS/MONTH:</label>
+                    <input
+                      className={styles.emailsInput}
+                      min={0}
+                      name='emails'
+                      onChange={({ target: { value } }) =>
+                        this.setState({ emails: value })
+                      }
+                      step={1}
+                      type='number'
+                      value={this.state.emails}
+                    />
+                  </span>
+                </div>
               ),
               features: [
-                <table className={styles.table} key='table'>
-                  <thead>
-                    <tr>
-                      <th>Lookups/mo</th>
-                      <th>Price per lookup</th>
-                      <th>Max price for tier</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {payg.tiers.map((tier, index) => (
-                      <tr key={tier.upTo}>
-                        <td>
-                          {index === 0 ? 0 : +payg.tiers[index - 1].upTo + 1} -{' '}
-                          {tier.upTo.replace('inf', '∞')}
-                        </td>
-                        <td>${formatPrice(tier.unitAmount)}</td>
-                        <td>
-                          {tier.upTo === 'inf'
-                            ? '∞'
-                            : `$${formatPrice(
-                                (+tier.upTo + 1) * +tier.unitAmount
-                              )}`}
-                        </td>
+                <div className={styles.tableWrapper} key='tableWrapper'>
+                  <table className={styles.table}>
+                    <thead className={styles.tableHead}>
+                      <tr>
+                        <th>Lookups/mo</th>
+                        <th>Price per lookup</th>
+                        <th>Max price for tier</th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>,
+                    </thead>
+                    <tbody>
+                      {payg.tiers.map((tier, index) => (
+                        <tr key={tier.upTo}>
+                          <td>
+                            {index === 0
+                              ? 0
+                              : numberWithCommas(
+                                  +payg.tiers[index - 1].upTo + 1
+                                )}{' '}
+                            - {numberWithCommas(tier.upTo).replace('inf', '∞')}
+                          </td>
+                          <td>${formatPrice(tier.unitAmount)}</td>
+                          <td>
+                            {tier.upTo === 'inf'
+                              ? '∞'
+                              : `$${formatPrice(
+                                  (+tier.upTo + 1) * +tier.unitAmount
+                                )}`}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>,
                 ...plans[1].features
               ],
               // Dynamic pricing.
               price: `$${formatPrice(
                 Math.round(calculatePrice(this.state.emails))
-              )}`
+              )}*`
             }}
           />
         </div>
+        <p className={styles.emailsWarning}>
+          * The price above is indicative. The price tier you fall in will be
+          determined by how many actual lookups you do for that month.
+        </p>
       </Section>
     )
   }
