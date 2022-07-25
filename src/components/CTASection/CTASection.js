7a04552eaec77241c79da39db0e758a0e9309f99
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-07-25 15:52:24 +0200

    chore: Fix dead images link

--- src/components/CTASection/CTASection.js
+++ src/components/CTASection/CTASection.js
@@ -17,19 +17,32 @@ export class CTASection extends Component {
           >
             <img
               className={styles.geekflare}
-              src='https://geekflare.com/wp-content/themes/geekflare/site/static/images/common/logos/header.png'
+              src='/assets/geekflare.webp'
             />
           </a>
+
           <a
-            href='https://bigmongolian.com/product/reacher'
+            href='https://www.producthunt.com/products/reacher#reacher-2'
             rel='noreferrer'
             target='_blank'
           >
             <img
-              className={styles.bigmongolian}
-              src='https://bigmongolian.com/wp-content/uploads/2020/09/9.5.jpg'
+              className={styles.producthunt}
+              src='/assets/producthunt.png'
             />
           </a>
+
+          <a
+            href=' https://www.blogdumoderateur.com/tools/reacher/'
+            rel='noreferrer'
+            target='_blank'
+          >
+            <img
+              className={styles.bdm}
+              src='/assets/bdm.png'
+            />
+          </a>
+
         </div>
         <a className={styles.cta} href='https://app.reacher.email/signup'>
           <CTAButton>Get started for free</CTAButton>
