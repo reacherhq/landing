4a9f2fb308faed4b579276129cf0fd90f947f672
Amaury M <1293565+amaurym@users.noreply.github.com>
2022-09-16 20:18:25 +0200

    fix: Improve lighthouse SEO

--- src/components/CTASection/CTASection.js
+++ src/components/CTASection/CTASection.js
@@ -15,7 +15,7 @@ export class CTASection extends Component {
             rel='noreferrer'
             target='_blank'
           >
-            <img className={styles.geekflare} src='/assets/geekflare.webp' />
+            <img alt="geekflare" className={styles.geekflare} src='/assets/geekflare.webp' />
           </a>
 
           <a
@@ -23,7 +23,7 @@ export class CTASection extends Component {
             rel='noreferrer'
             target='_blank'
           >
-            <img className={styles.producthunt} src='/assets/producthunt.png' />
+            <img alt="product-hunt" className={styles.producthunt} src='/assets/producthunt.png' />
           </a>
 
           <a
@@ -31,7 +31,7 @@ export class CTASection extends Component {
             rel='noreferrer'
             target='_blank'
           >
-            <img className={styles.bdm} src='/assets/bdm.png' />
+            <img alt="bdm" className={styles.bdm} src='/assets/bdm.png' />
           </a>
         </div>
         <a className={styles.cta} href='https://app.reacher.email/signup'>
